f3b543c109bd142eef0f3aec6a592ec6d441ef60
carterkioski <carterkioski@gmail.com>
2021-07-25 01:31:54 -0700

    add to app.py for sql alchemy flask

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
+from sqlalchemy import inspect, create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+import numpy as np
+import os
+import sqlalchemy
+import psycopg2
+
+
+engine = create_engine(os.environ['DATABASE_URL']) 
+connection = engine.connect()
+#Reflect an existing database and tables
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
 
 app = Flask(__name__)
 
